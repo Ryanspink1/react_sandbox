8cfd052aa652de9753294bd1324631f86a7c152d
Ryan Spink <ryanspinktech@gmail.com>
2018-04-10 19:49:43 -0600

    app.js and foodlist.js setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import FoodForm from './FoodForm';
+import FoodList from './FoodList';
+import { getFoods } from '../utils/requests';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      foods: []
+    }
+  }
+
+  componentDidMount() {
+    getFoods()
+    .then((foods) => this.setState({ foods: foods }))
+    .catch((error) => console.error({ error }))
+  }
+
   render() {
     return (
       <div className="app">
-        <h1 className="my-foods-header">Manage Foods</h1>
+        <FoodList foods={ this.state.food }/>
+        <Food Form />
       </div>
     );
   }
--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,2 +1,27 @@
 import React from 'react';
 import '../styles/FoodList.css';
+
+class FoodList extends Component {
+  let foods = this.props.foods;
+  let mappedFoods = foods.map((food) => {
+    return(
+      <div className="food-row">
+        <p className="food-name">{ food.name }</p>
+        <p className="food-calories">{ food.caloris }</p>
+      </div>
+    )
+  })
+  render() {
+    return (
+      <div className="food-list">
+        <div className="food-row title-row">
+          <p className="food-name title">Food</p>
+          <p className="food-calories title">Calories</p>
+        </div>
+        { mappedFoods }
+      </div>
+    )
+  }
+}
+
+export default FoodList;
